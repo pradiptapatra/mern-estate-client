592f94a20eaf0cc76f79913da069284cb8edb04b
pradipta <ppradiptapatra@gmail.com>
2024-08-08 23:49:11 +0530

    connection mongo db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotent from "dotenv";
 const app = express();
+dotent.config();
+mongoose.connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+ }).then(() => {
+    console.log("Connection to MongoDB!");
+ }).catch((err) => {
+    console.log(err);
+ });
 
 app.listen(3000, () => {
     console.log("Server is runing 3000");
